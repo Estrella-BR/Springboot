386498a7ab9321b87a6b853df5d23917ada03410
Estrella Blanco <estrellablanro@gmail.com>
2026-04-24 11:52:26 +0200

    Listado simple

---
+++ tutorial-springboot/src/main/java/com/ccsw/tutorial/TutorialSpringbootApplication.java
@@ -0,0 +1,18 @@
+package com.ccsw.tutorial;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorialSpringbootApplication implements CommandLineRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TutorialSpringbootApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) {
+
+    }
+}
