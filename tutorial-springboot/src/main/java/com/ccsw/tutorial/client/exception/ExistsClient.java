23ebcd0d4f08ebedbbc055657d6d8c61be309562
Estrella Blanco <estrellablanro@gmail.com>
2026-05-15 11:54:29 +0200

    Fix: Fix exceptions

--- tutorial-springboot/src/main/java/com/ccsw/tutorial/client/exception/ExistsClient.java
+++ tutorial-springboot/src/main/java/com/ccsw/tutorial/client/exception/ExistsClient.java
@@ -1,10 +1,5 @@
 package com.ccsw.tutorial.client.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(value = HttpStatus.NOT_ACCEPTABLE, reason = "El cliente ya existe")
-
 public class ExistsClient extends RuntimeException {
 
     public ExistsClient(String message) {
--- tutorial-springboot/src/main/java/com/ccsw/tutorial/loan/exception/GameLoaned.java
+++ tutorial-springboot/src/main/java/com/ccsw/tutorial/loan/exception/GameLoaned.java
@@ -1,9 +1,5 @@
 package com.ccsw.tutorial.loan.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "El ya está siendo prestado")
 public class GameLoaned extends RuntimeException {
 
     public GameLoaned(String message) {
